37981011112285977d2b59fbd7c8689538249d54
Julio Toribio <julioindigital@Julios-MacBook-Pro.local>
2025-07-29 00:18:15 -0500

    actualiza

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/recursos')
+def recursos():
+    return render_template('recursos.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
